ad4fdf419f075bfb443d271e1eeee3519884a948
Marcelo dos Santos Moraes Junior <marcelo.moraes@saeinfnt009.saedigital.local>
2018-05-24 21:00:51 -0300

    Added attraction many to many

--- source/core/models.py
+++ source/core/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from attractions.models import Attraction
 
 
 class TouristSpot(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     approved = models.BooleanField(default=False)
+    attractions = models.ManyToManyField(Attraction)
 
     def __str__(self):
         return self.name
