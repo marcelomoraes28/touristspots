35a622defd63daa54707f01eb0bab670e6e66676
Marcelo Moraes <marcelomoraes@MacBook-Pro-de-Marcelo.local>
2018-05-27 12:36:23 -0300

    Added new fields

--- source/core/models.py
+++ source/core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from attractions.models import Attraction
 from comments.models import Comment
 from evaluations.models import Evaluation
+from address.models import Address
 
 
 class TouristSpot(models.Model):
@@ -11,6 +12,8 @@ class TouristSpot(models.Model):
     attractions = models.ManyToManyField(Attraction)
     comments = models.ManyToManyField(Comment)
     evaluations = models.ManyToManyField(Evaluation)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE,
+                                null=True, blank=True)
 
     def __str__(self):
         return self.name
